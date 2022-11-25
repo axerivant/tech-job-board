4bb4844ac616284b168ea7218f697505595541d9
Axer Ivant <91627586+axerivant@users.noreply.github.com>
2022-11-25 13:07:40 -0800

    fixed: CSS in Result

--- src/features/search/internal/Result.svelte
+++ src/features/search/internal/Result.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import { goto } from '$app/navigation'
 	import { SkillsList, Title, Brief, Attribute, Action } from '.'
 
 	export let result: SearchResult
 
-	const { title, skills, brief, location, compensation } = result
+	const { title, skills, brief, location, compensation, postingID } = result
 </script>
 
 <button
-	class="text-left flex gap-6 bg-base-600 p-8 border border-accent-300 hover:brightness-110 transition-all"
+	on:click={() => goto(`/job/${postingID}`)}
+	class="text-left flex gap-6 bg-base-600 p-8 border border-accent-300 hover:brightness-110 transition-all h-64"
 >
 	<div class="flex flex-col gap-6 w-9/12">
 		<div class="flex flex-col gap-3">
@@ -16,7 +18,7 @@
 		</div>
 		<Brief {brief} />
 	</div>
-	<div class="w-3/12 flex flex-col gap-3 justify-between items-end">
+	<div class="w-3/12 flex flex-col justify-between items-end h-full">
 		<div>
 			<Attribute type="location" value={location} />
 			<Attribute type="compensation" value={compensation} />
